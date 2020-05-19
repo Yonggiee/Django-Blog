a5e7480f295025ad75cc8ef38b6c8d4242621bf1
Yonggiee <fongyongjie27@gmail.com>
2020-05-19 17:23:44 +0800

    add css for detailed_view

--- project/blog/comment/forms.py
+++ project/blog/comment/forms.py
@@ -6,3 +6,8 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs['style']='resize:none;'
+        self.fields['body'].widget.attrs['rows']=5
