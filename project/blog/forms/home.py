b32782545eef94d0eec759689ac2c9844fb1b23f
Yonggiee <fongyongjie27@gmail.com>
2020-05-20 11:04:58 +0800

    add errors to new post and edit post view

--- project/blog/forms/home.py
+++ project/blog/forms/home.py
@@ -1,12 +1,11 @@
 from django import forms
 
-
 class FilterForm(forms.Form):
-    date_from_day = forms.IntegerField(required=False)
-    date_from_month = forms.IntegerField(required=False)
-    date_from_year = forms.IntegerField(required=False)
-    date_to_day = forms.IntegerField(required=False)
-    date_to_month = forms.IntegerField(required=False)
-    date_to_year = forms.IntegerField(required=False)
+    date_from_day = forms.IntegerField(required=False, min_value=0, max_value=31)
+    date_from_month = forms.IntegerField(required=False, min_value=0, max_value=12)
+    date_from_year = forms.IntegerField(required=False, min_value=0)
+    date_to_day = forms.IntegerField(required=False, min_value=0, max_value=31)
+    date_to_month = forms.IntegerField(required=False, min_value=0, max_value=12)
+    date_to_year = forms.IntegerField(required=False, min_value=0)
     user = forms.CharField(required=False, widget=forms.TextInput(attrs={'class' : 'user-filter'}))
     title = forms.CharField(required=False)
