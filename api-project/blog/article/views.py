2d771039360c9c56ba43bc2ea545e28452749339
Yonggiee <fongyongjie27@gmail.com>
2020-05-24 15:33:07 +0800

    improved articles/1/comments/

--- api-project/blog/article/views.py
+++ api-project/blog/article/views.py
@@ -4,12 +4,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import Article
 from .serializers import ArticleSerializer, ArticleCommentSerializer
+from comment.serializers import CommentSerializer
 
 
 class ArticleList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   generics.GenericAPIView):
-    queryset = Article.objects.all()
+    queryset = Article.objects.all().order_by('-last_modified')
     serializer_class = ArticleSerializer
 
     def get(self, request, *args, **kwargs):
@@ -22,7 +23,7 @@ class ArticleDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
                     mixins.DestroyModelMixin,
                     generics.GenericAPIView):
-    queryset = Article.objects.all()
+    queryset = Article.objects.all()        ### why is queryset all when it is one object?
     serializer_class = ArticleSerializer
 
     def get(self, request, *args, **kwargs):
@@ -34,10 +35,13 @@ class ArticleDetail(mixins.RetrieveModelMixin,
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
-class CommentList(mixins.RetrieveModelMixin,
-                  generics.GenericAPIView):
+
+class CommentList(generics.GenericAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleCommentSerializer
 
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        comments = CommentSerializer(self.get_object().comments, many=True)
+        comments_json = comments.data
+        return Response(comments_json)
+        
