cd4629a9e757a91137613685110a533651b8e668
Yonggiee <fongyongjie27@gmail.com>
2020-05-27 13:04:26 +0800

    article detail page (using id)

--- api-project/blog-frontend/src/app/components/article-detail/article-detail.component.ts
+++ api-project/blog-frontend/src/app/components/article-detail/article-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-article-detail',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleDetailComponent implements OnInit {
 
-  constructor() { }
+  id;
+  article;
+
+  constructor(private route: ActivatedRoute, 
+    private articleService: ArticleService) { }
 
   ngOnInit(): void {
+    this.getIdFromParams();
+    this.getDetailedArticle();
   }
 
+  getIdFromParams() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id')
+    })
+  }
+
+  getDetailedArticle() {
+    this.articleService.getDetailedArticle(this.id).subscribe(
+      article => {
+        this.article = article;
+      }
+    )
+  }
 }
